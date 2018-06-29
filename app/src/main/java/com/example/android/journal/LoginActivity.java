04334bc093d4d18f84cbae2760e5770c02f60f33
Xilma <atiruo@gmail.com>
2018-06-29 15:38:20 +0300

    Working authentication with Firebase

--- app/src/main/java/com/example/android/journal/LoginActivity.java
+++ app/src/main/java/com/example/android/journal/LoginActivity.java
@@ -2,25 +2,129 @@ package com.example.android.journal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private AutoCompleteTextView emailTextView;
+    private EditText passwordEditView;
+    private TextView newUserTextView;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private Button loginButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailTextView = findViewById(R.id.email);
+        passwordEditView = findViewById(R.id.password);
+        newUserTextView = findViewById(R.id.signup_text);
+        newUserTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newUser = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(newUser);
+                finish();
+            }
+        });
+
+        progressBar = findViewById(R.id.login_progress);
+        loginButton = findViewById(R.id.email_sign_in_button);
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void signInEmail(View view) {
-        Intent main = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(main);
-        finish();
+    @Override
+    public void onStart(){
+        super.onStart();
+        //Check if user is signed in (non-null) and update
+    }
+
+    public void loginEmail(View view) {
+        String email = emailTextView.getText().toString().trim();
+        String password = passwordEditView.getText().toString().trim();
+
+        if(email.isEmpty()){
+            emailTextView.setError("Email is required");
+            emailTextView.requestFocus();
+            return;
+        }
+
+        //If email is invalid
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailTextView.setError("Please enter a valid email address");
+            emailTextView.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            passwordEditView.setError("Password is required");
+            passwordEditView.requestFocus();
+            return;
+        }
+
+        //If password length is less than 6
+        if(password.length()<8){
+            passwordEditView.setError("Password should be a minimum of 8 characters");
+            passwordEditView.requestFocus();
+            return;
+        }
+
+        showProgress();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgress();
+                        if (task.isSuccessful()) {
+                            // Sign in is successful
+                            Toast.makeText(LoginActivity.this, R.string.success_message,
+                                    Toast.LENGTH_SHORT).show();
+                            Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(main);
+                            finish();
+                        }
+
+                        // If sign in fails, display a message to the user.
+                        else {
+                              Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                });
+    }
+
+    public void hideProgress(){
+        progressBar.setVisibility(View.GONE);
+        newUserTextView.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.VISIBLE);
+    }
+
+    public void showProgress(){
+        progressBar.setVisibility(View.VISIBLE);
+        newUserTextView.setVisibility(View.GONE);
+        loginButton.setVisibility(View.GONE);
     }
 }
 
--- app/src/main/java/com/example/android/journal/SignUpActivity.java
+++ app/src/main/java/com/example/android/journal/SignUpActivity.java
@@ -1,16 +1,34 @@
 package com.example.android.journal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private AutoCompleteTextView tv_email_signup;
     private EditText tv_password_signup;
-    private Button signUp;
+    private TextView signinTextView;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,93 @@ public class SignUpActivity extends AppCompatActivity {
 
         tv_email_signup = findViewById(R.id.email_signup);
         tv_password_signup = findViewById(R.id.password_signup);
+        signinTextView = findViewById(R.id.signin_text);
+        signinTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent oldUser = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(oldUser);
+                finish();
+            }
+        });
 
+        progressBar = findViewById(R.id.signup_progress);
+        signupButton = findViewById(R.id.sign_up_button_email);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void registerUser(View view) {
+        String email = tv_email_signup.getText().toString().trim();
+        String password = tv_password_signup.getText().toString().trim();
+
+        if(email.isEmpty()){
+            tv_email_signup.setError("Email is required");
+            tv_email_signup.requestFocus();
+            return;
+        }
+
+        //If email is invalid
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            tv_email_signup.setError("Please enter a valid email address");
+            tv_email_signup.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            tv_password_signup.setError("Password is required");
+            tv_password_signup.requestFocus();
+            return;
+        }
+
+        //If password length is less than 6
+        if(password.length()<8){
+            tv_password_signup.setError("Password should be a minimum of 8 characters");
+            tv_password_signup.requestFocus();
+            return;
+        }
+
+        showProgress();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgress();
+                        if (task.isSuccessful()) {
+                            // Sign in is successful
+                            Toast.makeText(SignUpActivity.this, R.string.success_message,
+                                    Toast.LENGTH_LONG).show();
+                            Intent mainPage = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(mainPage);
+                            finish();
+                        }
+
+                        // If sign in fails, display a message to the user.
+                        else {
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(SignUpActivity.this, R.string.user_exists,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(SignUpActivity.this, Objects.requireNonNull(task.getException()).getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void hideProgress(){
+        progressBar.setVisibility(View.GONE);
+        signinTextView.setVisibility(View.VISIBLE);
+        signupButton.setVisibility(View.VISIBLE);
+    }
+
+    public void showProgress(){
+        progressBar.setVisibility(View.VISIBLE);
+        signinTextView.setVisibility(View.GONE);
+        signupButton.setVisibility(View.GONE);
     }
 }
