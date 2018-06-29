fd0b874dddc6770f31b96285cb5d0f6ef5016a32
Xilma <atiruo@gmail.com>
2018-06-29 12:31:43 +0300

    Added more layout changes

--- app/src/main/java/com/example/android/journal/SignUpActivity.java
+++ app/src/main/java/com/example/android/journal/SignUpActivity.java
@@ -1,13 +1,24 @@
 package com.example.android.journal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView tv_email_signup;
+    private EditText tv_password_signup;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        tv_email_signup = findViewById(R.id.email_signup);
+        tv_password_signup = findViewById(R.id.password_signup);
+
     }
 }
